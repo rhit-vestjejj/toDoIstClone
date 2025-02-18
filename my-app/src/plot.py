f30428961f3faf96895f95a3a26bc031efc50610
rhit-glazedh <glazedh@rose-hulman.edu>
2025-02-17 20:54:32 -0500

    legend added

--- my-app/src/plot.py
+++ my-app/src/plot.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-todo_tasks = pd.read_csv("tasks.cv")
+todo_tasks = pd.read_csv("tasks.csv")
 
 #Order of days
 days_ordered = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
@@ -11,11 +11,12 @@ tasks_not_done = ["tasks_not_done"]
 
 todo_tasks["Day"] = pd.Categorical(todo_tasks["Day"], categories=days_ordered, ordered = True)
 todo_tasks = todo_tasks.sort_values(by=["Day"], acending = "False" )
-plt.bar(days_ordered, tasks_done, width =0.45, label = "Complete", bottom = tasks_not_done)
-plt.bar(days_ordered,tasks_done, width = 0.45, label = "Incomplete" )
+plt.bar(days_ordered, tasks_done, width =0.45, label = "Complete", color = "blue",bottom = tasks_not_done)
+plt.bar(days_ordered,tasks_done, width = 0.45, label = "Incomplete", color = "red" )
 plt.ylabel("Total Tasks")
 plt.xlabel("Day of the week")
 
-
+plt.title("Daily Task Overview")
+plt.legend()
 
 plt.show()
