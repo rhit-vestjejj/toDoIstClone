ffae7d227eece59a3817d0dd840342e90c4d21bc
rhit-glazedh <glazedh@rose-hulman.edu>
2025-02-17 20:51:14 -0500

    ordered plots and data should be read and categorized properly

--- my-app/src/plot.py
+++ my-app/src/plot.py
@@ -6,9 +6,16 @@ todo_tasks = pd.read_csv("tasks.cv")
 
 #Order of days
 days_ordered = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+tasks_done = ["Tasks_Done"]
+tasks_not_done = ["tasks_not_done"]
+
 todo_tasks["Day"] = pd.Categorical(todo_tasks["Day"], categories=days_ordered, ordered = True)
-todo_tasks = todo_tasks.sort_values(by=["Day", "Tasks_Done", "Taks_Not_Done"], ascending = [True, False, False] )
-plt.bar(days_ordered)
-plt.bar
+todo_tasks = todo_tasks.sort_values(by=["Day"], acending = "False" )
+plt.bar(days_ordered, tasks_done, width =0.45, label = "Complete", bottom = tasks_not_done)
+plt.bar(days_ordered,tasks_done, width = 0.45, label = "Incomplete" )
+plt.ylabel("Total Tasks")
+plt.xlabel("Day of the week")
+
+
 
 plt.show()
