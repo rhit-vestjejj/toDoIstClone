499199f0e6063638e6588b1a7958ddc74126b717
rhit-glazedh <glazedh@rose-hulman.edu>
2025-02-17 20:35:38 -0500

    fixing csv file stuff and making days categorical

--- my-app/src/plot.py
+++ my-app/src/plot.py
@@ -2,9 +2,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-#Order of days
-days_order = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-fig, ax= plt.subplots()
 todo_tasks = pd.read_csv("tasks.cv")
+
+#Order of days
+days_ordered = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+todo_tasks["Day"] = pd.Categorical(todo_tasks["Day"], categories=days_ordered, ordered = True)
 todo_tasks = todo_tasks.sort_values(by=["Day", "Tasks_Done", "Taks_Not_Done"], ascending = [True, False, False] )
+plt.bar(days_ordered)
+plt.bar
+
 plt.show()
