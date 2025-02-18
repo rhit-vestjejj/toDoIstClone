8be4ffe441decb6c522c0f28be6f8fa1f1ef2d42
rhit-glazedh <glazedh@rose-hulman.edu>
2025-02-17 21:18:09 -0500

    fixed csv inputs

--- my-app/src/plot.py
+++ my-app/src/plot.py
@@ -8,20 +8,22 @@ data = {
     "Tasks_Not_Done": [2, 1, 3, 2, 1, 5, 6]
 }
 
+
 df_todo = pd.DataFrame(data)
 df_todo.to_csv("tasks.csv", index = False)
 todo_tasks = pd.read_csv("tasks.csv")
+print("Columns Names", todo_tasks.columns)
 
 
 
 
 #Order of days
 days_ordered = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-tasks_done = ["Tasks_Done"].values
-tasks_not_done = ["tasks_not_done"].values
+tasks_done = todo_tasks["Tasks_Done"].values
+tasks_not_done = todo_tasks["Tasks_Not_Done"].values  
 
 todo_tasks["Day"] = pd.Categorical(todo_tasks["Day"], categories=days_ordered, ordered = True)
-todo_tasks = todo_tasks.sort_values(by=["Day"], ascending = "False" )
+todo_tasks = todo_tasks.sort_values(by=["Day"], ascending = True )
 plt.bar(days_ordered, tasks_done, width =0.45, label = "Complete", color = "blue",bottom = tasks_not_done)
 plt.bar(days_ordered,tasks_done, width = 0.45, label = "Incomplete", color = "red" )
 plt.ylabel("Total Tasks")
